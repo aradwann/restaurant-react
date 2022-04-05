7653794cff923c0675fb7c7a3ea19c68a7190a0f
Ahmed Radwan <aradwann@protonmail.com>
2022-04-05 15:05:23 +0200

    add dishes list to App component state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/MenuComponent";
+import { DISHES } from "./shared/dishes";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+  render() {
+    return (
       <div>
-        <Navbar color="primary" dark expand="md">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </Navbar>
-        <Menu />
+        <div>
+          <Navbar color="primary" dark expand="md">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </Navbar>
+          <Menu dishes={this.state.dishes} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
